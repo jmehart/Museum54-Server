41d95fff997962e03e392680e181c3f61b127786
Jaimie Hart <hartjme@gmail.com>
2022-06-09 10:41:23 -0400

    fixtures and models done

--- app_api/models/__init__.py
+++ app_api/models/__init__.py
@@ -1,2 +1,7 @@
 from .curator import Curator
 from .artist import Artist
+from .art import Art
+from .classification import Classification
+from .genre import Genre
+from .medium import Medium
+from .style import Style
